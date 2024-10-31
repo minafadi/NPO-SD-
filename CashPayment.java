be43f86f112cc05f187afaea4be9ccfb6d041641
Shady Emad <Shadygirgis79@gmail.com>
2024-10-31 21:43:29 +0300

    Changed all Boolean function to boolean

---
+++ CashPayment.java
@@ -0,0 +1,26 @@
+public class CashPayment implements Payment {
+    private double tax;
+    private String description;
+
+    @Override
+    public boolean executePayment() {
+        throw new UnsupportedOperationException("Unimplemented method 'executePayment'");
+    }
+
+    @Override
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Payment.java
@@ -0,0 +1,4 @@
+public interface Payment {
+    public boolean executePayment();
+    public double getTax();
+}
