c3e8e9486fc956d6f638b296426785e09db7af32
Shady Emad <Shadygirgis79@gmail.com>
2024-11-06 03:48:53 +0200

    Drug List Page GUI bases are Done

---
+++ GUI_Page/DrugListPage.java
@@ -0,0 +1,19 @@
+package GUI_Page;
+
+import javax.swing.*;
+
+public class DrugListPage {
+    private JLabel DoctorLabel;
+    private JLabel SpecialLabel;
+    private JLabel BFLabel;
+    private JLabel DegreeLabel;
+    private JLabel GYLabel;
+    private JList DrugList;
+    private JComboBox DrugsCBox;
+    private JButton AddDrugButton;
+    private JComboBox PaymentCBOx;
+    private JComboBox DateCBox;
+    private JButton ReserveButton;
+    private JLabel DateLabel;
+    private JLabel PaymantLabel;
+}
