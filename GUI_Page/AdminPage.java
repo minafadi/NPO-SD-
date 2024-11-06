e674338b888653f33a89b0e6b566ce3ff67f5a6a
Shady Emad <Shadygirgis79@gmail.com>
2024-11-06 03:48:12 +0200

    Admin Dashboard Page GUI bases are Done

---
+++ GUI_Page/AdminPage.java
@@ -0,0 +1,17 @@
+package GUI_Page;
+
+import javax.swing.*;
+
+public class AdminPage {
+    private JTextField AddDoctorTF;
+    private JTextField AddAppointTF;
+    private JButton AddAppointButton;
+    private JButton AddDocButton;
+    private JTextField AddDrugTF;
+    private JButton AddDrugButton;
+    private JLabel AdminDashLabel;
+    private JButton DoneButton;
+    private JLabel AddDoctorLabel;
+    private JLabel AddAppointLabel;
+    private JLabel AddDrugLabel;
+}
