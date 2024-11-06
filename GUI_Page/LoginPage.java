409bf6935f8fbef2e6803cb151aebda9169aa00c
Shady Emad <Shadygirgis79@gmail.com>
2024-11-06 03:49:48 +0200

    Login Password modified

--- GUI_Page/LoginPage.java
+++ GUI_Page/LoginPage.java
@@ -4,11 +4,11 @@ import javax.swing.*;
 
 public class LoginPage extends JFrame {
     private JTextField NameTF;
-    private JTextField PasswordTF;
     private JComboBox OccupationCBox;
     private JButton LoginButton;
     private JLabel NameLabel;
     private JLabel PasswordLabel;
     private JLabel OccupationLabel;
     private JLabel ForgetLabel;
+    private JPasswordField PasswordPF;
 }
