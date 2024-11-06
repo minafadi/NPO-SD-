ad3e4a1e1c25395c73f46d58568a5cc256d89509
Shady Emad <Shadygirgis79@gmail.com>
2024-11-06 03:49:16 +0200

    In Voice Page GUI bases are Done

---
+++ GUI_Page/InVoicePage.java
@@ -0,0 +1,18 @@
+package GUI_Page;
+
+import javax.swing.*;
+
+public class InVoicePage {
+    private JLabel InVoiceLabel;
+    private JTextField PatientTF;
+    private JTextField DoctorTF;
+    private JComboBox DrugsCBox;
+    private JComboBox PMCBox;
+    private JTextPane AmountTP;
+    private JButton PayButton;
+    private JLabel PatientLabel;
+    private JLabel DoctorLabel;
+    private JLabel DrugsLabel;
+    private JLabel PaymentLabel;
+    private JLabel AmountLabel;
+}
