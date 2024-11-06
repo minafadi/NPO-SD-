4d7805103c155b0f8cb47229af51cd339c7be65e
Shady Emad <Shadygirgis79@gmail.com>
2024-11-06 02:17:11 +0200

    Login Page GUI bases are Done

---
+++ GUI_Page/LoginPage.java
@@ -0,0 +1,14 @@
+package GUI_Page;
+
+import javax.swing.*;
+
+public class LoginPage extends JFrame {
+    private JTextField NameTF;
+    private JTextField PasswordTF;
+    private JComboBox OccupationCBox;
+    private JButton LoginButton;
+    private JLabel NameLabel;
+    private JLabel PasswordLabel;
+    private JLabel OccupationLabel;
+    private JLabel ForgetLabel;
+}
