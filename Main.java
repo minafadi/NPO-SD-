2a69d56cbd812abdf5fbe99a9d000293293ed179
matthew7402 <105174746+matthew7402@users.noreply.github.com>
2024-11-09 13:13:18 +0200

    Adding drugs

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import Classes.DB;
+import GUI_Page.AddDrug;
 import GUI_Page.DoctorsListPage;
 import GUI_Page.LoginPage;
 import Classes.*;
@@ -13,10 +14,12 @@ public class Main {
         System.out.println("Connected to database");
         DoctorsListPage d = new DoctorsListPage();
         //d.setVisible(true);
-        LoginPage l = new LoginPage();
-        l.setVisible(true);
+        //LoginPage l = new LoginPage();
+        //l.setVisible(true);
         //Patient p = new Patient("matwa","1234");
         //Patient p2 = new Patient("karim","1234",22,true,"1234");
-
+        //Drug drug = new Drug("Augmantin",150,23.5);
+        AddDrug ad = new AddDrug();
+        ad.setVisible(true);
     }
 }
