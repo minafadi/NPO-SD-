20b9c5b96c16757d45bbe8d49db456deb63f94ce
Shady Emad <Shadygirgis79@gmail.com>
2025-01-16 21:46:27 +0200

    Modification in Symptoms classes to work with template DB

--- Models/ShortnessOfBreath.java
+++ Models/ShortnessOfBreath.java
@@ -3,11 +3,38 @@ package Models;
 public class ShortnessOfBreath extends Symptom {
     public ShortnessOfBreath(Illness illness) {
         this.illness = illness;
-        this.illness.addDrug(Drug.readAllDrugs("ShortnessOfBreath"));
     }
+
+    @Override
+    protected void diagnose() {
+        illness.diagnose();
+        System.out.println("Adding diagnosis for shortness of breath...");
+    }
+
+    @Override
+    protected void prescribeDrugs() {
+        illness.prescribeDrugs(); // Delegate to the wrapped illness
+        System.out.println("Adding prescription for shortness of breath...");
+        Drug drug = Drug.readAllDrugs("ShortnessOfBreath");
+        if (drug != null) {
+            illness.addDrug(drug); // Add the drug only if it's not null
+        }
+        else {
+            System.out.println("No specific drugs found for Shortness of breath Symptom.");
+        }
+    }
+
+    @Override
+    protected double calculateTreatmentCost() {
+        // Delegate to the wrapped illness
+        System.out.println("Adding additional cost for Shortness of breath...");
+        return illness.calculateTreatmentCost() + 150;
+    }
+
     public String getDescription() {
-        return super.getDescription()+"ShortnessOfBreath";
+        return super.getDescription() + " Shortness of Breath, ";
     }
+
     @Override
     public int severity() {
         return illness.severity() + 4;
@@ -15,6 +42,6 @@ public class ShortnessOfBreath extends Symptom {
 
     @Override
     public double calculateCost() {
-        return illness.calculateCost() + 150;
+        return calculateTreatmentCost();
     }
 }
